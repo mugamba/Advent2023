d9089aa03a602ba5cf5fa2c0479eada4bf45cb59
Mate Sosa <mate.sosa@pakel.hr>
2023-12-01 15:48:04 +0100

    Advent 2023 / Puzzle 1

---
+++ Puzzle01/Task2/Task/Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task
+{
+    class Program
+    {
+
+
+   
+
+
+
+        static void Main(string[] args)
+        {
+
+           var t =  "text".LastIndexOf("t");
+
+            var lines = File.ReadAllLines("input.txt").Select(o => String.Join("", o.ToCharArray().Where(c => ).ToArray())).ToList();
+            //var listOfAll = new List<int>(); 
+
+            //foreach (var line in lines)
+            //{
+            //    var num1 = line.First();
+            //    var num2 = line.Last();
+            //    listOfAll.Add(Int32.Parse(num1.ToString() + num2.ToString()));
+            //}
+
+
+        //var maxElf = list.Max();
+
+
+
+        //Console.WriteLine("Result is {0}", listOfAll.Sum());
+        Console.ReadKey();
+        }
+    }
+}
