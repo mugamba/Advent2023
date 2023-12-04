3446b28385503b54bc50539990d2b4ae37d9ee7e
Mate Sosa <mate.sosa@pakel.hr>
2023-12-04 09:15:58 +0100

    Puzzle04

---
+++ Puzzle04/Task1/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+var lines = File.ReadAllLines("input.txt");
+var listOfAll = new List<int>();
+
+foreach (var line in lines)
+{
+    line.Split(":");
+    
+    var splits = line.Split(":");
+    var setSplits = splits[1].Split("|");
+
+    var drawn = setSplits[0].Split(" ").Select(o=>o.Trim()).Where(o=>!String.IsNullOrWhiteSpace(o));
+    var wining = setSplits[1].Split(" ").Select(o => o.Trim()).Where(o => !String.IsNullOrWhiteSpace(o));
+    var hits=  drawn.Intersect(wining).Count();
+
+    if (hits > 0)
+     listOfAll.Add((int)Math.Pow(2, hits - 1));
+}
+
+//var filtered = listOfAll.Where(o => o.Sets.All(c => c.Blue <= 14 && c.Red <= 12 && c.Green <= 13)).ToList();
+Console.WriteLine(listOfAll.Sum());
+Console.ReadKey();
+
+
---
+++ Puzzle04/Task2/Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+var lines = File.ReadAllLines("input.txt");
+var listOfAll = new List<int>();
+var dict = new Dictionary<int, int>();
+
+for (int i = 1; i < lines.Length+1; i++)
+    dict.Add(i, 1);
+
+
+int counter = 1;
+foreach (var line in lines)
+{
+    var repNumber = dict[counter];
+    line.Split(":");
+    var splits = line.Split(":");
+    var setSplits = splits[1].Split("|");
+
+    var drawn = setSplits[0].Split(" ").Select(o => o.Trim()).Where(o => !String.IsNullOrWhiteSpace(o));
+    var wining = setSplits[1].Split(" ").Select(o => o.Trim()).Where(o => !String.IsNullOrWhiteSpace(o));
+    var hits = drawn.Intersect(wining).Count();
+
+    for (int i = 0; i < hits; i++)
+    {
+        dict[counter + i + 1] = dict[counter + i + 1] + repNumber;
+    }
+    
+    counter++;
+}
+
+//var filtered = listOfAll.Where(o => o.Sets.All(c => c.Blue <= 14 && c.Red <= 12 && c.Green <= 13)).ToList();
+Console.WriteLine(dict.Select(o=>o.Value).Sum());
+Console.ReadKey();
+
+
