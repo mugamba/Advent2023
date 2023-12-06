53925a0cae3f3596cb98223ce6e0d5ac07f830ca
Mate Sosa <mate.sosa@pakel.hr>
2023-12-06 08:46:41 +0100

    Puzzle06

---
+++ Puzzle06/Task1/Task/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+var lines = File.ReadAllLines("input.txt");
+
+var firstLine = lines.First().Replace("Time:", "").Trim();
+var lastLine = lines.Last().Replace("Distance:", "").Trim();
+
+
+var firstlineSplits = firstLine.Split(" ").Where(o=>!String.IsNullOrWhiteSpace(o)).ToArray();
+var lastLineSplits = lastLine.Split(" ").Where(o => !String.IsNullOrWhiteSpace(o)).ToArray();
+
+
+var listOfallRaces = new List<BoatRace>();
+
+for (var i = 0; i < firstlineSplits.Length; i++)
+{ 
+    var boat = new BoatRace(int.Parse(firstlineSplits[i].Trim()), int.Parse(lastLineSplits[i].Trim()));
+
+    listOfallRaces.Add(boat);
+}
+
+var result = 1;
+foreach (var boat in listOfallRaces)
+{
+    boat.GetPossibleOutcomes();
+
+
+    result = result * boat.possibleOutcomes.Where(o => o._distance > boat._distanceToBeat).Count();
+}
+
+
+
+
+
+Console.WriteLine(result);
+Console.ReadKey();
+
+
+public class BoatRace
+{
+
+    public int _time;
+    public int _distanceToBeat;
+
+    public BoatRace(int time, int distanceToBeat)
+    {
+
+        possibleOutcomes = new List<PossibleOutcomes>();
+        _time = time;
+        _distanceToBeat = distanceToBeat;
+    }
+
+    public List<PossibleOutcomes> possibleOutcomes;
+
+    public BoatRace GetClone()
+    {
+        return (BoatRace)this.MemberwiseClone();
+    }
+
+    public void GetPossibleOutcomes()
+    { 
+        for(int i = 1;i<= _time;i++) {
+            possibleOutcomes.Add(new PossibleOutcomes(i, i * (_time - i)));
+        }
+    }
+
+}
+
+public class PossibleOutcomes
+{
+    public int _buttonHold;
+    public int _distance;
+
+    public PossibleOutcomes(int buttonHold, int distance)
+    { 
+        _buttonHold = buttonHold;
+        _distance = distance;
+    }
+
+}
+
---
+++ Puzzle06/Task2/Task/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+var lines = File.ReadAllLines("input.txt");
+
+var firstLine = lines.First().Replace("Time:", "").Trim();
+var lastLine = lines.Last().Replace("Distance:", "").Trim();
+
+
+var firstlineSplits = firstLine.Split(" ").Where(o=>!String.IsNullOrWhiteSpace(o)).ToArray();
+var lastLineSplits = lastLine.Split(" ").Where(o => !String.IsNullOrWhiteSpace(o)).ToArray();
+
+
+var listOfallRaces = new List<BoatRace>();
+
+
+    var boat = new BoatRace(long.Parse(String.Join("", firstlineSplits.Select(o=>o.Trim()))), 
+        long.Parse(String.Join("", lastLineSplits.Select(o => o.Trim()))));
+
+    
+    long result = 0;
+
+    boat.GetFailedOutcomes();
+    result = boat._time - boat.failedOutcomes.Count();
+
+Console.WriteLine(result);
+Console.ReadKey();
+
+
+public class BoatRace
+{
+
+    public long _time;
+    public long _distanceToBeat;
+
+    public BoatRace(long time, long distanceToBeat)
+    {
+
+        failedOutcomes = new List<PossibleOutcomes>();
+        _time = time;
+        _distanceToBeat = distanceToBeat;
+    }
+
+    public List<PossibleOutcomes> failedOutcomes;
+
+    public BoatRace GetClone()
+    {
+        return (BoatRace)this.MemberwiseClone();
+    }
+
+    public void GetPossibleOutcomes()
+    { 
+        for(long i = 1;i<= _time;i++) {
+            failedOutcomes.Add(new PossibleOutcomes(i, i * (_time - i)));
+        }
+    }
+
+    public void GetFailedOutcomes()
+    {
+        for (long i = 1; i <= _time; i++)
+        {
+            if (i * (_time - i) > _distanceToBeat)
+                break;
+
+
+            failedOutcomes.Add(new PossibleOutcomes(i, i * (_time - i)));
+        }
+
+        for (long i = _time; i >= 0; i--)
+        {
+            if (i * (_time - i) > _distanceToBeat)
+                break;
+
+            failedOutcomes.Add(new PossibleOutcomes(i, i * (_time - i)));
+        }
+
+    }
+
+
+}
+
+public class PossibleOutcomes
+{
+    public long _buttonHold;
+    public long _distance;
+
+    public PossibleOutcomes(long buttonHold, long distance)
+    { 
+        _buttonHold = buttonHold;
+        _distance = distance;
+    }
+
+}
+
